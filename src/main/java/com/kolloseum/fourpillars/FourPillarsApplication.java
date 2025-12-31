ea383761021a22bc4a4781a13cc21d117749bcbb
wook <wook0126@gmail.com>
2025-12-31 11:13:44 +0900

    fix redis debug level

--- src/main/java/com/kolloseum/fourpillars/FourPillarsApplication.java
+++ src/main/java/com/kolloseum/fourpillars/FourPillarsApplication.java
@@ -4,8 +4,10 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
+
 @EnableAdminServer
-@SpringBootApplication
+@SpringBootApplication(exclude = { RedisRepositoriesAutoConfiguration.class })
 public class FourPillarsApplication {
 
     public static void main(String[] args) {
