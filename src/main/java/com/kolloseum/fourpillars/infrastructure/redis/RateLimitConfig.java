25cfd56659a717b62ac6c792addd7cf263d6bdb4
wook <wook0126@gmail.com>
2026-01-07 11:55:13 +0900

    fix several bugs

--- src/main/java/com/kolloseum/fourpillars/infrastructure/redis/RateLimitConfig.java
+++ src/main/java/com/kolloseum/fourpillars/infrastructure/redis/RateLimitConfig.java
@@ -12,10 +12,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RateLimitConfig {
 
-    @Value("${spring.data.redis.host}")
+    @Value("${spring.data.redis.host:localhost}")
     private String redisHost;
 
-    @Value("${spring.data.redis.port}")
+    @Value("${spring.data.redis.port:6379}")
     private int redisPort;
 
     @Bean
